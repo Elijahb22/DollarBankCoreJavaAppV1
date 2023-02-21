f39c06af59489fdae86792f287671d64bb8cbea5
elijahb22 <elibusick11@gmail.com>
2023-02-21 12:04:35 -0500

    add userid,password, int id, password and intitial deposit to custoemr model

--- src/com/dollarsbank/model/Customer.java
+++ src/com/dollarsbank/model/Customer.java
@@ -1,16 +1,48 @@
 package com.dollarsbank.model;
 
 public class Customer {
-    
+	
+    private int id;
 	private  String name;
-     private String address;
-     private  int phoneNumber;
-     
-	public Customer(String name, String address, int phoneNumber) {
+    private String address;
+    private  int phoneNumber;
+    private int user_id;
+    private String password;
+    private int deposit;
+    
+	public Customer(int id, String name, String address, int phoneNumber, int user_id, String password, int deposit) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
+		this.user_id = user_id;
+		this.password = password;
+		this.deposit = deposit;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(int deposit) {
+		this.deposit = deposit;
 	}
 	public String getName() {
 		return name;
@@ -29,6 +61,12 @@ public class Customer {
 	}
 	public void setPhoneNumber(int phoneNumber) {
 		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber
+				+ ", user_id=" + user_id + ", password=" + password + ", deposit=" + deposit + "]";
 	}
      
 	
