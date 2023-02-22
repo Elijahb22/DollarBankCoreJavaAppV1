e5509e65472e5ac5868ca593e2405de906e59bda
elijahb22 <elibusick11@gmail.com>
2023-02-22 04:32:18 -0500

    fix customer model

--- src/com/dollarsbank/model/Customer.java
+++ src/com/dollarsbank/model/Customer.java
@@ -1,48 +1,16 @@
 package com.dollarsbank.model;
 
 public class Customer {
-	
-    private int id;
-	private  String name;
-    private String address;
-    private  int phoneNumber;
-    private int user_id;
-    private String password;
-    private int deposit;
-    
-	public Customer(int id, String name, String address, int phoneNumber, int user_id, String password, int deposit) {
+	private String name;
+	private String address;
+	private String contact_number;
+	private String user_id;
+	public Customer(String name, String address, String contact_number, String user_id) {
 		super();
-		this.id = id;
 		this.name = name;
 		this.address = address;
-		this.phoneNumber = phoneNumber;
+		this.contact_number = contact_number;
 		this.user_id = user_id;
-		this.password = password;
-		this.deposit = deposit;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getDeposit() {
-		return deposit;
-	}
-	public void setDeposit(int deposit) {
-		this.deposit = deposit;
 	}
 	public String getName() {
 		return name;
@@ -56,20 +24,18 @@ public class Customer {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public int getPhoneNumber() {
-		return phoneNumber;
+	public String getContact_number() {
+		return contact_number;
 	}
-	public void setPhoneNumber(int phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	public void setContact_number(String contact_number) {
+		this.contact_number = contact_number;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
 	}
 	
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber
-				+ ", user_id=" + user_id + ", password=" + password + ", deposit=" + deposit + "]";
-	}
-     
 	
-	
-
 }
